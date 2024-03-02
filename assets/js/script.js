db3a1a74d8f250d3018f4240260d319734b27acf
fuadcode <fuadgi@code.edu.az>
2024-03-03 01:05:08 +0400

    JS_FUNCTION_TASK

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+//Task-1
+
+// function SumNumberStrenght(numbers) {
+//     let num = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+       
+//         if (numbers[i] % 2 === 0 && typeof numbers[i] === 'number') {
+//             num += Math.pow(numbers[i], 2);              //Math.pow methodun internetden search edərək yazdım..
+//         }
+//     }
+
+//     return num;
+// }
+// const numArr = [14, 3, 1, 6, 10, 5, 12, 7];
+// const stage = SumNumberStrenght(numArr);
+// console.log(stage);
+
+
+//Task-2
+
+// function CheckMonth(arr) {
+//     return arr.map((month, index) => `${month}-${index + 1}`);
+//   }
+  
+//   const months = ["Yanvar", "Fevral", "Mart", "Aprel"];
+//   const result = CheckMonth(months);
+//   console.log(result);
